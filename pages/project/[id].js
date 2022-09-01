38738c8dcd07e261c547da250d7d16942326c427
Turing Jobs <turing.algeria@gmail.com>
2022-09-01 10:24:33 +0100

    submit project form

--- pages/project/[id].js
+++ pages/project/[id].js
@@ -1,4 +1,15 @@
-import { Button, Card, Spin, Tag, Typography } from "antd";
+import { FileZipOutlined, InboxOutlined } from "@ant-design/icons";
+import {
+  Button,
+  Card,
+  Form,
+  PageHeader,
+  Spin,
+  Tag,
+  Typography,
+  Upload,
+} from "antd";
+import TextArea from "antd/lib/input/TextArea";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
@@ -6,6 +17,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { getJobDetails } from "../../reducers/jobdetailsReducer";
 import Styles from "./projectdetails.module.css";
 const { Title, Paragraph } = Typography;
+const { Dragger } = Upload;
+
 function ProjectDetails() {
   const router = useRouter();
   const dispatch = useDispatch();
@@ -24,6 +37,29 @@ function ProjectDetails() {
   console.log("----------- job details ", jobDetailsResult);
   console.log("----------- router query ", router.query);
 
+  const props = {
+    name: "file",
+    multiple: true,
+    action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+
+    onChange(info) {
+      const { status } = info.file;
+
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+
+      if (status === "done") {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+
+    onDrop(e) {
+      console.log("Dropped files", e.dataTransfer.files);
+    },
+  };
   return (
     <div className={Styles.wrapper}>
       {isFetchingJob ? (
@@ -43,6 +79,12 @@ function ProjectDetails() {
               <Head>
                 <title>{jobDetailsResult?.title}</title>
               </Head>
+              <PageHeader
+                className={Styles.pageheader}
+                onBack={() => null}
+                title="Project details"
+              />
+
               <Card
                 className={Styles.project_card}
                 title={jobDetailsResult?.title}
@@ -93,6 +135,39 @@ function ProjectDetails() {
 
                   <br />
                 </div>
+              </Card>
+
+              <Card
+                style={{ margin: "2rem auto ", maxWidth: "900px" }}
+                title="Submit project"
+                actions={[
+                  <Button style={{ width: "90%" }} type="primary">
+                    {" "}
+                    Submit
+                  </Button>,
+                ]}
+              >
+                <Form layout="vertical" style={{ margin: "1rem auto" }}>
+                  <Form.Item label="Comment :">
+                    <TextArea
+                      placeholder="comment ..."
+                      style={{ margin: "1rem auto", padding: "1rem" }}
+                    ></TextArea>
+                  </Form.Item>
+                  <Form.Item label="Upload files :">
+                    <Dragger style={{}} {...props}>
+                      <p className="ant-upload-drag-icon">
+                        <FileZipOutlined />
+                      </p>
+                      <p
+                        className="ant-upload-text"
+                        style={{ padding: "2rem" }}
+                      >
+                        Click or drag file to this area to upload
+                      </p>
+                    </Dragger>
+                  </Form.Item>
+                </Form>
               </Card>
             </>
           )}
